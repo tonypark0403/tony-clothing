1cb937e38a6b3d614a3c70d388ed5f355aad6c7d
Tony Park <tonypark0403@gmail.com>
2019-08-19 00:32:31 -0400

    added add and remove items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -13,3 +13,8 @@ export const clearItemFromCart = item => ({
   type: ActionTypes.cart.CLEAR_ITEM_FROM_CART,
   payload: item
 });
+
+export const removeItem = item => ({
+  type: ActionTypes.cart.REMOVE_ITEM,
+  payload: item
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import ActionTypes from "../action-types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -24,6 +24,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: state.cartItems.filter(
           cartItem => cartItem.id !== action.payload.id
         )
+      };
+    case ActionTypes.cart.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
     default:
       return state;
