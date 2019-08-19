95a1cd8e204dfc62d01bb83eb62f3ecd2f2cd6d8
Tony Park <tonypark0403@gmail.com>
2019-08-18 20:12:13 -0400

    added quantity in data

--- src/redux/action-types.js
+++ src/redux/action-types.js
@@ -3,7 +3,8 @@ const ActionTypes = {
     SET_CURRENT_USER: "SET_CURRENT_USER"
   },
   cart: {
-    TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN"
+    TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+    ADD_ITEM: "ADD_ITEM"
   }
 };
 
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import ActionTypes from "../action-types";
 export const toggleCartHidden = () => ({
   type: ActionTypes.cart.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+  type: ActionTypes.cart.ADD_ITEM,
+  payload: item
+});
