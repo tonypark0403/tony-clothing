a5a5de65cb7a21e8a136bd5983272c668d710412
Tony Park <tonypark0403@gmail.com>
2019-08-19 00:16:39 -0400

    implemented whole items remove regardless of numbers

--- src/redux/action-types.js
+++ src/redux/action-types.js
@@ -4,7 +4,8 @@ const ActionTypes = {
   },
   cart: {
     TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
-    ADD_ITEM: "ADD_ITEM"
+    ADD_ITEM: "ADD_ITEM",
+    CLEAR_ITEM_FROM_CART: "CLEAR_ITEM_FROM_CART"
   }
 };
 
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
   type: ActionTypes.cart.ADD_ITEM,
   payload: item
 });
+
+export const clearItemFromCart = item => ({
+  type: ActionTypes.cart.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
