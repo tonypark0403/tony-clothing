68c74b786a31db213e9ce0ac878201373c5dc6d9
Tony Park <tonypark0403@gmail.com>
2019-08-22 01:09:42 -0400

    fixed another directory issue

--- src/components/cart/cart-icon/cartIcon.js
+++ src/components/cart/cart-icon/cartIcon.js
@@ -3,9 +3,10 @@ import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingIcon } from "../../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../../redux/cart/cart.actions";
-import "./cart-icon.scss";
 import { selectCartItemsCount } from "../../../redux/cart/cart.selectors";
 import { createStructuredSelector } from "reselect";
+
+import "./cart-icon.scss";
 
 const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
