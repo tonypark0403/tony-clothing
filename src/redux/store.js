38191dbcabb310af20f4dcc52c594e32177585a6
Tony Park <tonypark0403@gmail.com>
2019-08-22 23:43:21 -0400

    added development environment for middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,21 +5,28 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import helloMiddleWare from "./middleware/hello";
 
-const isWindowExtension = false;
-const reduxDevtoolsExtension =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+let isWindowExtension = true;
+let reduxDevtoolsExtension;
 
 //array
-const middlewares = [logger, helloMiddleWare];
+const middlewares = [];
 
-export const store = isWindowExtension
-  ? createStore(
-      rootReducer,
-      compose(
-        applyMiddleware(...middlewares),
-        reduxDevtoolsExtension
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+  middlewares.push(helloMiddleWare);
+  reduxDevtoolsExtension =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+export const store =
+  isWindowExtension && process.env.NODE_ENV === "development"
+    ? createStore(
+        rootReducer,
+        compose(
+          applyMiddleware(...middlewares),
+          reduxDevtoolsExtension
+        )
       )
-    )
-  : createStore(rootReducer, applyMiddleware(...middlewares));
+    : createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
